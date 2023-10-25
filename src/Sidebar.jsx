dd8423242726b6b6ea5c0bacdfec9d357927cfc6
RedJyu <piotr.stezycki@protonmai.com>
2023-10-25 14:32:39 +0200

    sidebar pre css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,38 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './Context';
+import sublinks from './data';
 function Sidebar() {
-  return <h2>Sidebar</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className='sidebar'>
+      <div className='sidebar-container'>
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((item) => {
+            const { pageId, page, links } = item;
+            console.log(item);
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 }
 export default Sidebar;
